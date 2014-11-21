41ea76461107eace4b908bc26795242a22f9f1cb
Sasamus <labus93@gmail.com>
2014-11-21 15:58:12 +0100

    Added member variables and declared functions

--- src/BlockingQueue.h
+++ src/BlockingQueue.h
@@ -11,17 +11,37 @@
 #define BLOCKINGQUEUE_H_
 
 #include <vector>
+#include <mutex>
+#include <condition_variable>
 
 template <class T>
 class BlockingQueue {
 
 private:
+	//A queue
 	std::vector<T> m_queue = new std::vector<T>();
+
+	//The size of the queue
 	int m_size;
+
+	//A mutex to lock the critical sections
+	std::mutex m_mutex;
+
+	//A condition variable to lock the critical sections
+	std::condition_variable m_condition_variable;
+
 
 public:
 	BlockingQueue(int size);
 	virtual ~BlockingQueue();
+
+	T Take();
+	//Pre:
+	//Post: Returns element at the start of m_queue
+
+	void Put();
+	//Pre:
+	//Post: Puts and element at the back of m_queue
 };
 
 #endif /* BLOCKINGQUEUE_H_ */
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+//=============================================================
+// Name         : main.cpp
+// Author       : Albin Engström
+// Created      : 2014-11-21
+// Modified     : 2014-11-21
+// Description  : main for project my-blocking-queue
+// Purpose      : Start the program
+//=============================================================
+
+
+using namespace std;
+
+int main()
+{
+
+
+
+    return 0;
+}
