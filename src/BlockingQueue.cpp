a52da98286ecf41f0eaa4887fae651cf142ff964
Sasamus <labus93@gmail.com>
2014-11-21 11:26:55 +0100

    Created an Eclipse project

---
+++ src/BlockingQueue.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : BlockingQueue.cpp
+// Author      : Albin Engström
+// Created     : 2014-11-21
+// Modified    : 2014-11-21
+// Description : Implementation of class BlockingQueue
+//============================================================================
+
+#include "BlockingQueue.h"
+
+BlockingQueue::BlockingQueue() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BlockingQueue::~BlockingQueue() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/BlockingQueue.h
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : BlockingQueue.h
+// Author      : Albin Engström
+// Created     : 2014-11-21
+// Modified    : 2014-11-21
+// Description : Declaration of class BlockingQueue
+// Purpose	   : My own C++ version of java's Blocking Queue
+//============================================================================
+
+#ifndef BLOCKINGQUEUE_H_
+#define BLOCKINGQUEUE_H_
+
+class BlockingQueue {
+public:
+	BlockingQueue();
+	virtual ~BlockingQueue();
+};
+
+#endif /* BLOCKINGQUEUE_H_ */
